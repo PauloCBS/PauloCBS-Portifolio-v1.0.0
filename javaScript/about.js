0773cb77990416279c009f6a2763d3419a0e0021
Your Name <paulo-cesar-batista@hotmail.com>
2022-08-02 20:47:18 +0100

    About section with a interactive timeline

---
+++ javaScript/about.js
@@ -0,0 +1,38 @@
+function year(ano){
+
+    if(ano == experience17){
+        
+        let test = document.getElementById('experience')
+        test.style.display = "block"
+        test.innerHTML = " I started studying Agronomy at Federal University of Vicosa the best university for agricultural studies in Brazil"
+
+    }
+    else if(ano == experience19){
+
+        let test = document.getElementById('experience')
+        
+        test.className = "text-white"
+        test.innerHTML = " I had the opotunity to participate at The Ohio Program and do an Internship Abroad with landscaping at Double eagle golf Club. The internship was sponsored by the Ohio State University." 
+        
+    }
+    else if(ano == experience20){
+        let test = document.getElementById('experience')
+        test.style.display = "inline"
+        test.innerHTML = "I Started studying system analisys and devlopment and working at Tecocomp as Apple Support specialist"
+    }
+    else if(ano == experience21){
+        let test = document.getElementById('experience')
+        test.style.display = "inline"
+        test.innerHTML = " This was mas first year as service desk at Tecnocomp working for Afry Brasil, United States of America and Canada. Besides, I decided to dive into the development world and since then I´m in love for front-end devlopment.  "
+
+    }
+    else if(ano == experience22){
+        let test = document.getElementById('experience')
+         test.style.display = "inline"
+        test.innerHTML = "I decided to live in Lisbon, and started to working for FAS as a helpdesk technician for NOS."
+    }
+    
+    
+
+    
+}
