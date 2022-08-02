ed2d6ee1a545c00bd4b518564aebfc038d6c3ab9
Your Name <paulo-cesar-batista@hotmail.com>
2022-08-02 14:22:57 +0100

    Home typying effects and style added using javascript and css

---
+++ javaScript/home.js
@@ -0,0 +1,21 @@
+async function typeSentence(sentence, eleRef, delay = 100) {
+    const letters = sentence.split("");
+    let i = 0;
+    while(i < letters.length) {
+      await waitForMs(delay);
+      $(eleRef).append(letters[i]);
+      i++
+      
+    }
+    return;
+  
+  }
+  
+  
+  function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+  
+  typeSentence("Paulo Silva", "#sentence")
+  
+      
